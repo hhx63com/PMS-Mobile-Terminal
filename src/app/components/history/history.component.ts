6f82d8c1204a31b7060f9796dcc976eeec65025d
hhx63com <hhx63com@example.com>
2026-04-26 14:24:59 +0800

    将历史记录页面改为Privacy Policy页面

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HistoryItem } from '../../models/history-item.model';
-import { HistoryService } from '../../services/history.service';
-import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-history',
@@ -12,30 +9,9 @@ import { ImageService } from '../../services/image.service';
   styleUrl: './history.component.css'
 })
 export class HistoryComponent implements OnInit {
-  history: HistoryItem[] = [];
 
-  constructor(
-    private historyService: HistoryService,
-    private imageService: ImageService
-  ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    console.log('HistoryComponent ngOnInit called');
-    this.loadHistory();
-  }
-
-  loadHistory(): void {
-    console.log('loadHistory called');
-    this.history = this.historyService.getHistory();
-    console.log('Loaded history:', this.history);
-  }
-
-  getImageUrl(item: any): string {
-    return this.imageService.getImageUrl(item);
-  }
-
-  clearHistory(): void {
-    this.historyService.clearHistory();
-    this.history = [];
   }
 }
